f277788dd8bbc53746c057d9574c0f8f764d8cb1
Gabi Iezzi <geiezzi90@gmail.com>
2023-03-16 17:00:11 -0300

    add junit import - fix maven plugin

--- src/test/java/com/egg/biblioteca/services/impl/LibroServiceImplUnitTest.java
+++ src/test/java/com/egg/biblioteca/services/impl/LibroServiceImplUnitTest.java
@@ -1,10 +1,13 @@
 package com.egg.biblioteca.services.impl;
 
+import com.egg.biblioteca.entities.Autor;
 import com.egg.biblioteca.entities.Libro;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import java.time.LocalDateTime;
 
@@ -23,6 +26,11 @@ class LibroServiceImplUnitTest {
         libro1.setTitulo("El señor de los anillos");
         libro1.setCantEjemplares(1000);
         libro1.setIsbn(3490349L);
+
+        Autor autor = new Autor();
+        autor.setId("1213");
+        autor.setNombre("Robert Robertson");
+
         libro1.setAutor(null);
         libro1.setAutor(null);
         libro1.setAlta(LocalDateTime.now());
@@ -31,6 +39,11 @@ class LibroServiceImplUnitTest {
         Libro libroGuardado = libroService.crearLibro(libro1);
 
         //Assert
+
+        assertEquals(libro1.getIsbn(), libroGuardado.getIsbn());
+        assertEquals(libro1.getAlta(), libroGuardado.getAlta());
+        assertEquals(libro1.getCantEjemplares(), libroGuardado.getCantEjemplares());
+
 
 
 
